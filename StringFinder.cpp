23d32d114654a1bf65fb3b25da8d63a449a9d474
Christopher Plonski <christopherplonski@gmail.com>
2024-09-25 10:04:33 -0500

    Creating StringFinder CPP and Header

---
+++ StringFinder.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "StringFinder.hpp"
+
+using namespace std;
+
+int StringFinder::find_in_string(const string baseString, char charToFindInString)
+{
+
+
+	return -1;
+}
+
+int StringFinder::find_in_string(const string baseString, string stringToFindInString)
+{
+
+	return -1;
+}
